b5864ced7e23f90fc49e970049a1b512f84d2514
unknown <k.shin0102@gmail.com>
2021-06-20 20:52:57 +0900

    行間スペース調整

--- test/neutral.py
+++ test/neutral.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python
 
 import RPi.GPIO as GPIO
@@ -6,10 +5,9 @@ import time
 import Adafruit_PCA9685
 
 GPIO.setmode(GPIO.BOARD)
-
 GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-pwm = Adafruit_PCA9685.PCA9685()
+pwm=Adafruit_PCA9685.PCA9685()
 pwm.set_pwm_freq(60)
 
 ################################
