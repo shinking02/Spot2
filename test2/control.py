29ba4c0d85d411e8f7190fff1c4d4ec784ef07d3
unknown <k.shin0102@gmail.com>
2021-06-23 18:50:04 +0900

    微修正

--- test2/control.py
+++ test2/control.py
@@ -9,12 +9,9 @@ pwm=Adafruit_PCA9685.PCA9685()
 pwm.set_pwm_freq(50)
 
 def moveServo(id, degree, speed):
-<<<<<<< HEAD
     pwm.set_pwm(0, 0, 295)
 
-=======
     adj_list = [295, 305, 380, 280, 320, 260, 300, 285, 100, 170, 130, 500]
     if speed == 0:
         pwm.set_pwm(id, 0, adj_list[id] + degree)
         
->>>>>>> be8e2f88802e9a4764d7f5da933c11a5cf6f4f51
