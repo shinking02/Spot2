8c9636eaa2acd0ec5782ef5f40a75102ed78140f
unknown <k.shin0102@gmail.com>
2021-06-23 18:19:24 +0900

    配列数値追加

--- test2/control.py
+++ test2/control.py
@@ -9,4 +9,5 @@ pwm=Adafruit_PCA9685.PCA9685()
 pwm.set_pwm_freq(50)
 
 def moveServo(id, degree, speed):
-    adj_list = []
+    adj_list = [295, 305, 380, 280, 320, 260, 300, 285, 100, 170, 130, 500]
+    
