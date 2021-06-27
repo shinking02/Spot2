b801f4af475bb83e6e61798b1c4b4e8434077dcc
kta123 <keitagrace35@gmail.com>
2021-06-27 16:13:43 +0900

    a

--- test2/set_n.py
+++ test2/set_n.py
@@ -1,1 +1,19 @@
+from control import moveServo
+from time import sleep
+import neutral
+
+neutral.n()
+sleep(2)
+moveServo(0, -100, 100)
+moveServo(1, -100, 100)
+moveServo(2, -100, 100)
+moveServo(3, -100, 100)
+moveServo(4, -100, 100)
+moveServo(5, -100, 100)
+moveServo(6, -100, 100)
+moveServo(7, -100, 100)
+moveServo(8, -100, 100)
+moveServo(9, -100, 100)
+moveServo(10, -100, 100)
+moveServo(11, -100, 100)
 
