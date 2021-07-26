dc73e1596a482f7af04dbdf687f4f83a59afb7dc
unknown <k.shin0102@gmail.com>
2021-07-26 19:48:39 +0900

    a

--- test2/control.py
+++ test2/control.py
@@ -12,6 +12,7 @@ pwm.set_pwm_freq(50)
 
 now = [295, 305, 380, 258, 200, 380, 200, 395, 200, 380, 230, 400]
 adj_list = [295, 305, 380, 258, 200, 380, 200, 395, 200, 380, 230, 400]
+sleep_list = [295, 305, 380, 258, 212, 368, 230, 365, 80, 500, 110, 520]
 
 def moveServo(id, degree, speed):
     if speed == 0:
@@ -27,33 +28,9 @@ def moveServo(id, degree, speed):
             now[id] -= 1
             sleep(0.0001 * speed)
 
-def moveServos(id, degree, speed):
-    if speed == 0:
-        pwm.set_pwm(id, 0, adj_list[id] + degree)
-        now[id] += degree
-    while(now[id] != adj_list[id] + degree):
-        if now[id] < adj_list[id] + degree:
-            pwm.set_pwm(id, 0, now[id] + 1)
-            now[id] += 1
-            sleep(0.0001 * speed)
-        else:
-            pwm.set_pwm(id, 0, now[id] - 1)
-            now[id] -= 1
-            sleep(0.0001 * speed)
-
 def quick_sleep():
-    moveServo(0, 0, 0)
-    moveServo(1, 0, 0)
-    moveServo(2, 0, 0)
-    moveServo(3, 0, 0)
-    moveServo(4, 12, 0)
-    moveServo(5, -12, 0)
-    moveServo(6, 30, 0)
-    moveServo(7, -30, 0)
-    moveServo(8, -120, 0)
-    moveServo(9, 120, 0)
-    moveServo(10, -120, 0)
-    moveServo(11, 120, 0)
+    for i in range(11):
+        moveServo(i, sleep_list[i], 0)
 
 def set_nt():
     while(now[4] != adj_list[4]):
@@ -81,4 +58,4 @@ def set_nt():
         now[9] -= 1
         sleep(0.005)
 
-    def set_sleep():
+#def set_sleep():
